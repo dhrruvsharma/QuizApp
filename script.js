e32cd18b549d4961acbb83eafee7617d88b883fd
Dhruv Sharma <dsd71213@gmail.com>
2023-10-23 16:20:58 +0530

    The username gets retained in the localStorage

--- script.js
+++ script.js
@@ -1,73 +1,80 @@
+
 function sendName() {
     const inputValue = document.getElementById("name").value;
-    localStorage.setItem("username", inputValue);
+    localStorage.setItem("retain_username", inputValue);
     window.location.href = "game.html";
 }
-const nm = localStorage.getItem("username");
+const nm = localStorage.getItem("retain_username");
 document.addEventListener('DOMContentLoaded', function () {
-    window.addEventListener("beforeunload", () => {
-        localStorage.clear();
-    })
-    const navButtons = document.querySelector(".numberContainer");
     const displayName = document.getElementById("displayName");
     displayName.innerText = nm;
+    window.addEventListener("beforeunload", () => {
+        clearItems();
+    })
+    const navButtons = document.querySelector(".numberContainer");
     const choices = Array.from(document.getElementsByClassName("choice-text"));
     console.log(choices);
     let questions = [{
         question: "Inside which HTML element we put the JavaScript ?",
-        choices:["<script>","<javascript>","<js>","<scripting>"],
+        choices: ["<script>", "<javascript>", "<js>", "<scripting>"],
         answer: "<script>"
     },
     {
         question: "What is the correct syntax for referring to an external script?",
-        choices: ["<script href='xx.js'>","<script name='xx.js>'","<script src='xx.js'>","<script file='xx.js'"],
+        choices: ["<script href='xx.js'>", "<script name='xx.js>'", "<script src='xx.js'>", "<script file='xx.js'"],
         answer: "<script src='xx.js'>"
     },
     {
         question: "How do you write Hello World in alert box?",
-        choices: ["msgBox('Hello World')","alertBox('Hello World')","msg('Hello World')","alert('Hello World')"],
+        choices: ["msgBox('Hello World')", "alertBox('Hello World')", "msg('Hello World')", "alert('Hello World')"],
         answer: "alert('Hello World')"
     },
     {
         question: "What type of language is javascript?",
-        choices: ["Object-Orineted","Object Based","Procedural","None of the Above"],
+        choices: ["Object-Orineted", "Object Based", "Procedural", "None of the Above"],
         answer: "Object-Orinted"
     },
     {
         question: "Which of the following keyword is used to declare a variable in javascript?",
-        choices: ["var","let","Both of the above","None of the above"],
+        choices: ["var", "let", "Both of the above", "None of the above"],
         answer: "Both of the above"
     },
     {
         question: "Which of the following method is used to access HTML elements in javascript?",
-        choices: ["getElementbyId()","getElementbyClassName()","Both of the above","None of the above"],
+        choices: ["getElementbyId()", "getElementbyClassName()", "Both of the above", "None of the above"],
         answer: "Both of the above"
     },
     {
         question: "Upon encountering empty statements, what does the Javascript interpreter do?",
-        choices: ["Throws an error","Ignores the statement","Gives a warning","None of the above"],
+        choices: ["Throws an error", "Ignores the statement", "Gives a warning", "None of the above"],
         answer: "Ignores the statement"
     },
     {
         question: "How are constant variables declared in javascript?",
-        choices: ["const","var","let","constant"],
+        choices: ["const", "var", "let", "constant"],
         answer: "const"
     },
     {
         question: "Which of the following is not javascript data type ?",
-        choices: ["Null","Undefined","Number","All of the above"],
+        choices: ["Null", "Undefined", "Number", "All of the above"],
         answer: "All of the above"
     },
     {
         question: "Which of the following scoping does javascript use ?",
-        choices: ["Sequential","Segmental","Lexical","Literal"],
+        choices: ["Sequential", "Segmental", "Lexical", "Literal"],
         answer: "Lexical"
     }
     ]
     const score = 0;
     const correct = 4;
     const wrong = -1;
-
+    function clearItems() {
+        for (let key in localStorage){
+            if(!key.startsWith("retain_")){
+                localStorage.removeItem(key);
+            }
+        }
+    }
     const question = document.getElementById("question");
     let currentQuestionIndex = 0;
     const choiceElement = document.querySelector(".choice-container");
@@ -79,7 +86,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const currentquestion = questions[currentQuestionIndex];
         question.innerText = currentquestion.question;
         choiceFieldset.innerHTML = "";
-        currentquestion.choices.forEach((choice,index) => {
+        currentquestion.choices.forEach((choice, index) => {
             const div = document.createElement('div');
             div.className = "choice-div";
             const choiceInput = document.createElement('input');
@@ -90,32 +97,32 @@ document.addEventListener('DOMContentLoaded', function () {
             choiceInput.className = "choice-class";
 
             const storedChoice = localStorage.getItem(`selectedChoice_${currentQuestionIndex}`);
-            
-            if (storedChoice === choice){
+
+            if (storedChoice === choice) {
                 choiceInput.checked = true;
             }
 
             const choiceLabel = document.createElement("label");
-            choiceLabel.setAttribute("for",`choice${index}`);
+            choiceLabel.setAttribute("for", `choice${index}`);
             choiceLabel.className = "choice-label";
             choiceLabel.textContent = choice;
             choiceFieldset.append(div);
             div.append(choiceInput);
             div.append(choiceLabel);
 
-            choiceInput.addEventListener("change" , () => {
-                localStorage.setItem(`selectedChoice_${currentQuestionIndex}`,choiceInput.value);
+            choiceInput.addEventListener("change", () => {
+                localStorage.setItem(`selectedChoice_${currentQuestionIndex}`, choiceInput.value);
             })
         })
     }
     prev.addEventListener('click', () => {
-        if (currentQuestionIndex > 0){
+        if (currentQuestionIndex > 0) {
             currentQuestionIndex--;
             displayQuestion();
         }
     })
     next.addEventListener('click', () => {
-        if (currentQuestionIndex < questions.length - 1){
+        if (currentQuestionIndex < questions.length - 1) {
             currentQuestionIndex++;
             displayQuestion();
         }
@@ -125,28 +132,28 @@ document.addEventListener('DOMContentLoaded', function () {
         currentQuestionIndex = questionNumber;
         displayQuestion();
     }
-    navButtons.addEventListener("click" ,(event) => {
+    navButtons.addEventListener("click", (event) => {
         if (event.target.matches("button[data-question-number]")) {
-            const questionNumber = parseInt(event.target.getAttribute("data-question-number"),10);
+            const questionNumber = parseInt(event.target.getAttribute("data-question-number"), 10);
             goToquestion(questionNumber);
         }
     })
-    for (let i = 0;i < questions.length; i++){
+    for (let i = 0; i < questions.length; i++) {
         const button = document.createElement("button");
-        button.textContent = `${i+1}`;
+        button.textContent = `${i + 1}`;
         button.className = "number-buttons";
-        button.setAttribute("data-question-number",i);
+        button.setAttribute("data-question-number", i);
         navButtons.append(button);
     }
     function updateStyle() {
         const but = document.querySelectorAll(".number-buttons");
-        but.forEach((b , index) => {
+        but.forEach((b, index) => {
             const hasAttempted = localStorage.getItem(`selectedChoice_${index}`) !== null;
             const isMarked = localStorage.getItem(`reviewedQuestion_${index}`);
             if (hasAttempted) {
                 b.style.backgroundColor = "green";
             }
-            if(isMarked){
+            if (isMarked) {
                 b.style.backgroundColor = "red";
             }
             else {
@@ -164,12 +171,11 @@ document.addEventListener('DOMContentLoaded', function () {
             localStorage.removeItem(reviewKey);
         }
         else {
-            localStorage.setItem(reviewKey,"true");
+            localStorage.setItem(reviewKey, "true");
         }
-        console.log(localStorage);
         updateStyle();
     }
-    const markButton  = document.getElementById("mark");
+    const markButton = document.getElementById("mark");
     markButton.addEventListener("click", () => {
         mark(currentQuestionIndex);
     })
